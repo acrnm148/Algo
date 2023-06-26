925bbda7825be05ca08ac04706d05cfa549bd6f1
sunakang <67724306+acrnm148@users.noreply.github.com>
2023-06-26 20:44:10 +0900

    Update JadenCase 문자열 만들기.java

--- 강수나/programmers/JadenCase 문자열 만들기.java
+++ 강수나/programmers/JadenCase 문자열 만들기.java
@@ -2,31 +2,26 @@ import java.io.;
 import java.util.;
 
 class Solution {
-    
-      1. split으로 문자열 배열로 저장
-          1. 각각 for문 돌면서
-              1. 첫 문자가 a~z일 때 A~Z로 변경
-              2. 뒷 문자들이 A~Z일 때 a~z로 변경
-     
+
     public String solution(String s) {
         StringBuilder sb = new StringBuilder();
         int len = s.length();
         char last = '0';
-        for (int i=0; ilen; i++) {
+        for (int i=0; i<len; i++) {
             char c = s.charAt(i);
-            if (i==0 && (c='a' && c='z')) {
+            if (i==0 && (c='a' && c='z')) { //첫 문자(시작) && 소문자 => 대문자
                 sb.append((char)(c-32));
             } 
-            else if (i!=0 && last==' ' && (c='a' && c='z')) {
+            else if (i!=0 && last==' ' && (c='a' && c='z')) { //첫 문자 && 소문자 => 대문자
                 sb.append((char)(c-32));
             }
-            else if (i!=0 && last!=' ' && (c='A' && c='Z')) {
+            else if (i!=0 && last!=' ' && (c='A' && c='Z')) { //첫 문자X && 대문자 => 소문자
                 sb.append((char)(c+32));
             }
             else {
                 sb.append(c);
             }
-            last = c;
+            last = c; //이전 문자가 공백인지 확인하기 위함
         }
 
         System.out.println(sb);
