b54b4ff0751e6ebf3ca4f11d9d2671a16fa48a59
sunakang <67724306+acrnm148@users.noreply.github.com>
2023-08-11 06:43:34 +0900

    Create 점프와 순간이동.java

---
+++ 강수나/programmers/점프와 순간이동.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.io.*;
+
+public class Solution {
+    
+    public int solution(int n) {
+        int ans = 0;
+        
+        while (n>0) {
+            if (n % 2 ==0) {
+                n/=2;
+            } else {
+                n--;
+                ans++;
+            }
+        }
+
+        return ans;
+    }
+}
